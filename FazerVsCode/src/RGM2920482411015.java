6f33b767829a4d36d07819ad0fd06dd557edc09d
Matheus Filipe <math.filipe23@gmail.com>
2024-09-09 18:12:07 -0300

    [feat] change-file

---
+++ FazerVsCode/src/RGM2920482411015.java
@@ -0,0 +1,20 @@
+public class RGM2920482411015 {
+
+    public static void main(String[] args) {
+        String nomeCompleto = "Matheus Filipe Morais Cavalcanti";
+        String rgm = "2920482411015";
+        int idade = 23;
+        String gitHub = "@MathFilipeDev"; 
+        String linkedIn = "@mathfilipe";
+        String curso = "Análise e desenvolvimento de sistemas"; 
+        String turma = "Segundo semestre";
+
+        System.out.println("Nome Completo: " + nomeCompleto);
+        System.out.println("RGM: " + rgm);
+        System.out.println("Idade: " + idade);
+        System.out.println("GitHub: " + gitHub);
+        System.out.println("LinkedIn: " + linkedIn);
+        System.out.println("Curso: " + curso);
+        System.out.println("Turma: " + turma);
+    }
+}
